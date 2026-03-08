86c4da0f6f68d4712a1c1768de8e3185ac5043cc
max <maxtclinton@gmail.com>
2026-03-08 13:06:29 -0400

    wire index.ts to cli main entry point

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,7 @@
  * hotbuns - Bitcoin full node in TypeScript (Bun)
  */
 
-import * as os from "os";
-import * as path from "path";
-
-function parseArgs(argv: string[]): { datadir: string } {
-  const defaultDatadir = path.join(os.homedir(), ".hotbuns");
-  let datadir = defaultDatadir;
-
-  for (let i = 0; i < argv.length; i++) {
-    const arg = argv[i];
-    if (arg === "--datadir" && argv[i + 1]) {
-      datadir = argv[i + 1];
-      break;
-    } else if (arg.startsWith("--datadir=")) {
-      datadir = arg.slice("--datadir=".length);
-      break;
-    }
-  }
-
-  return { datadir };
-}
-
-async function main(): Promise<void> {
-  console.log("hotbuns v0.1.0 starting...");
-
-  const args = parseArgs(Bun.argv);
-  console.log(`Data directory: ${args.datadir}`);
-}
+import { main } from "./cli/cli.js";
 
 main().catch((err) => {
   console.error("Fatal error:", err);
